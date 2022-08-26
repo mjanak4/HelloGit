e9c7e455cf4c1e174ab14b8bf271b58f46969ae9
mjana <mjana@10.64.101.48>
2022-08-26 12:31:46 -0500

    added missing semicolon

--- src/Hello.java
+++ src/Hello.java
@@ -13,7 +13,7 @@ public class Hello {
 		System.out.println("Michael Janak");
 		System.out.println("Computer Science!");
 		System.out.println("Zach kozak");
-		System.out.println("Also computer science")
+		System.out.println("Also computer science");
 		
 	}
 }
