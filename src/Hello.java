a216893074ce3ff961c0ad1e1a1dd81298f79693
mjana <mjana@10.69.86.82>
2022-08-25 21:34:27 -0500

    added my major

---
+++ src/Hello.java
@@ -0,0 +1,17 @@
+/**
+ * Author: Michael Janak (mjanak4@huskers.unl.edu) 
+ * Date: 08/22/2022
+ * 
+ * A simple "Hello World!" program in Java
+ * 
+ * @author mjanak
+ *
+ */
+public class Hello {
+
+	public static void main(String args[]) {
+		System.out.println("Michael Janak");
+		System.out.println("Computer Science!");
+		
+	}
+}
